a0667349c0aea648af4e6c271e3d08d49eba9e9e
OSCAR FABIAN ESPANA INAGAN <oscar.espana@epn.edu.ec>
2019-07-19 17:37:01 -0500

    Cambio de clases con respecto a la nueva base de datos

--- src/main/java/epn/edu/ec/entidades/AdolescenteInfractorCAI.java
+++ src/main/java/epn/edu/ec/entidades/AdolescenteInfractorCAI.java
@@ -3,6 +3,7 @@ package epn.edu.ec.entidades;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
+import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -15,6 +16,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
@@ -65,5 +67,6 @@ public class AdolescenteInfractorCAI implements Serializable {
     public void setObservacionIngreso(String observacionIngreso) {
         this.observacionIngreso = observacionIngreso;
     }
+
     
 }
--- src/main/java/epn/edu/ec/entidades/DetalleInfraccionCAI.java
+++ src/main/java/epn/edu/ec/entidades/DetalleInfraccionCAI.java
@@ -1,6 +1,7 @@
 package epn.edu.ec.entidades;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -10,10 +11,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 @Entity
 @Table(name = "t_deta_infraccion_cai")
@@ -133,6 +136,4 @@ public class DetalleInfraccionCAI implements Serializable {
     public void setIdAdolescenteInfractorCAI(AdolescenteInfractorCAI idAdolescenteInfractorCAI) {
         this.idAdolescenteInfractorCAI = idAdolescenteInfractorCAI;
     }
-
-    
 }
